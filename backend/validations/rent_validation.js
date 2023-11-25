c0125f1b37c904fd245b0ac33c24b77e81404359
Andres Felipe Cano Colorado <142123685+AndresCan0@users.noreply.github.com>
2023-11-24 23:27:58 -0500

    Delete backend directory

--- backend/validations/rent_validation.js
+++
@@ -1,9 +0,0 @@
-const { body } = require('express-validator');
-
-const createRentValidation = [
-    body('platenumber').notEmpty().withMessage('La placa del automóvil es obligatoria'),
-    body('initialdate').notEmpty().withMessage('La fecha inicial es obligatoria'),
-    body('finaldate').notEmpty().withMessage('La fecha final es obligatoria'),
-];
-
-module.exports = {createRentValidation};
--- backend/validations/returncar_validation.js
+++
@@ -1,9 +0,0 @@
-const { body } = require('express-validator');
-
-const createReturncarValidation = [
-    body('rentnumber').notEmpty().withMessage('El número de renta es obligatorio'),
-    body('platenumber').notEmpty().withMessage('La placa del automóvil es obligatoria'),
-    body('returndate').notEmpty().withMessage('La fecha de devolución es obligatoria'),
-];
-
-module.exports = {createReturncarValidation};
